d8d5f63fc76fabc05aea603c4b6a10223dee2257
Ram Babu <ram.kottapally@gmail.com>
2021-01-22 17:23:26 +0530

    Updated RG regex to match with other galleries

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
     function is_valid_url_RG(url) {
         /* m.ragalahari.com -- is for mobile. So accept if url has either www./m.  */
-        return /^(http(s)?:\/\/)((www|m)\.)?ragalahari.com\/(movie|actor|actress)\//.test(url);
+        return /^(http(s)?:\/\/)((www|m)\.)?ragalahari.com\/(movie(s)?|actor|actress|localevents)\//.test(url);
     }
 
     /* Function that does some CSS manipulation when given URL is valid */
