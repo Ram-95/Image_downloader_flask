d9d5cab3ee4d2d17a144314428349a1bdd19628f
Ram Babu <ram.kottapally@gmail.com>
2021-01-28 21:14:51 +0530

    Add download view and modified other views

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request, render_template, send_from_directory, url_for
+from flask import Flask, escape, request, render_template, send_from_directory, url_for, send_file
 import IB_scrapper as IB
 import os
 import RG_scrapper as RG
@@ -22,8 +22,12 @@ def Idlebrain():
             print(f'\nURL: {url}\n')
             IB_obj = IB.IB(url)
             invalid_url = IB_obj.start()
-            #print(f'INVALID URL: {invalid_url}')
-            return 'True' if invalid_url else 'False'
+            zip_file_name = os.path.basename(IB_obj.imgs_dir) + '.zip'
+            if not invalid_url:
+                print(zip_file_name)
+                return {'status': 'False', 'filename': zip_file_name}
+                #return ('False', zip_file_name)
+            return {'status': 'True'}
     else:
         error = 'Invalid URL'
     return render_template('IB.html', error=error)
@@ -36,8 +40,13 @@ def ragalahari():
         url = request.args.get("url")
         if url is not None:
             print(f'\nURL: {url}\n')
-            invalid_url = RG.start(url)
-            return 'True' if invalid_url else 'False'
+            (invalid_url, dir_name) = RG.start(url)
+            zip_file_name = os.path.basename(dir_name) + '.zip'
+            if not invalid_url:
+                print(zip_file_name)
+                return {'status': 'False', 'filename': zip_file_name}
+                #return ('False', zip_file_name)
+            return {'status': 'True'}
     else:
         error = 'Invalid URL'
     return render_template('RG.html', error=error)
@@ -50,8 +59,14 @@ def cinejosh():
         url = request.args.get("url")
         if url is not None:
             print(f'\nURL: {url}\n')
-            invalid_url = CJ.start(url)
-            return 'True' if invalid_url else 'False'
+            (invalid_url, dir_name) = CJ.start(url)
+            zip_file_name = os.path.basename(dir_name) + '.zip'
+            if not invalid_url:
+                print(zip_file_name)
+                return {'status': 'False', 'filename': zip_file_name}
+                #return ('False', zip_file_name)
+            return {'status': 'True'}
+            
     else:
         error = 'Invalid URL'
     return render_template('CJ.html', error=error)
@@ -75,6 +90,24 @@ def bst():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
+
+
+@app.route('/download', methods = ['GET', 'POST'])
+def download():
+    if request.method == 'GET':
+        file_path = request.args.get("filepath")
+        if file_path is not None:
+            #print(f'\n\nFile_path: {file_path}\n\n')
+            path = os.path.join(os.getcwd(), file_path)
+            #print(f'\n\nFilepath: {path}\n\n')
+            return send_file(path, as_attachment=True)
+        else:
+            return "<p>Error</p>"
+    else:
+        return "<h3>Internal Error!</h3>"
+
+
 
 
 @app.route('/hello')
